f38b1129a75fd8bf3207e53624d506bb86ffe83e
Rajendra Mehra <razmehra@hotmail.com>
2019-02-05 15:32:31 +0530

    First Commit

---
+++ SPDInfoApp/SPDInfoApp/Models/SPDInfo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SPDInfoApp.Models
+{
+   public class SPDInfo
+    {
+        public int Appid { get; set; }
+        public string AppearingClass { get; set; }
+        public int ApplicationID { get; set; }
+        public string StudentName { get; set; }
+        public int RollNo { get; set; }
+        public int EnrolmentNo { get; set; }
+        public DateTime DOB { get; set; }
+        public string Medium { get; set; }
+        public string Gender { get; set; }
+        public string Category { get; set; }
+        public string RegCastCertificate { get; set; }
+        public bool IsHandicapped { get; set; }
+        public string HandicappDetail { get; set; }
+        public string BloodGroup { get; set; }
+        public string PhoneMobile { get; set; }
+        public string SSSMId { get; set; }
+        public string AadharNo { get; set; }
+        public string EMail { get; set; }
+        public string AddressPermanent { get; set; }
+        public string AddressCurrent { get; set; }
+        public bool IsUrban { get; set; }
+        public string NativePlace { get; set; }
+        public string RegNativeCertificateNo { get; set; }
+        public string FHName { get; set; }
+        public string MotherName { get; set; }
+        public string PhoneMobile_Gardian { get; set; }
+        public double IncomeFather { get; set; }
+        public string OccupationFather { get; set; }
+        public string BankAcNo { get; set; }
+        public string BankIFSC { get; set; }
+        public string BankName { get; set; }
+        public string BankBranch { get; set; }
+        public string VoterID { get; set; }
+        public string PANNo { get; set; }
+        public string DrivingLicNo { get; set; }
+        public string ScholershipName { get; set; }
+    }
+}
